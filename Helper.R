42e75a003e017a17dcc32f8d671494678d39ee32
Clemens Hahn <clemens@couchbits.com>
2021-11-11 13:06:47 +0100

    use read-file from SDK

---
+++ Helper.R
@@ -0,0 +1,36 @@
+#
+# Just a helper for local development. will no be bundled into the Docker MoveApps-App
+#
+
+# for loading move CSV files
+library(move)
+
+readInput <- function(sourceFile) {
+  input <- NULL
+  if(!is.null(sourceFile) && sourceFile != "") {
+    if (file.info(sourceFile)$size == 0) {
+        # handle the special `null`-input
+        logger.warn("The App has received invalid input! It cannot process NULL-input. Aborting..")
+        stop("The App has received invalid input! It cannot process NULL-input. Check the output of the preceding App or adjust the datasource configuration.")
+    }
+    logger.debug("Loading file from %s", sourceFile)
+    input <- tryCatch({
+        # 1: try to read input as move RDS file
+        readRDS(file = sourceFile)
+      },
+      error = function(readRdsError) {
+        tryCatch({
+          # 2 (fallback): try to read input as move CSV file
+          move(sourceFile, removeDuplicatedTimestamps=TRUE)
+        },
+        error = function(readCsvError) {
+          # collect errors for report and throw custom error
+          stop(paste(sourceFile, " -> readRDS(sourceFile): ", readRdsError, "move(sourceFile): ", readCsvError, sep = ""))
+        })
+      })
+  } else {
+    logger.debug("Skip loading: no source File")
+  }
+
+  input
+}

e2c8755199a81364f0845a851abfd9c3a4f83d28
Clemens Hahn <clemens@couchbits.com>
2021-11-11 13:07:10 +0100

    use read-file from SDK

--- RFunction.R
+++ RFunction.R
@@ -1,30 +1,7 @@
 # for loading move CSV files
 library(move)
-
-readLocalFile <- function(sourceFile) {
-  input <- NULL
-  if(!is.null(sourceFile) && sourceFile != "") {
-    logger.debug("Loading file from %s", sourceFile)
-    input <- tryCatch({
-      # 1: try to read input as move RDS file
-      readRDS(file = sourceFile)
-    },
-    error = function(readRdsError) {
-      tryCatch({
-        # 2 (fallback): try to read input as move CSV file
-        move(sourceFile, removeDuplicatedTimestamps=TRUE)
-      },
-      error = function(readCsvError) {
-        # collect errors for report and throw custom error
-        stop(paste(sourceFile, " -> readRDS(sourceFile): ", readRdsError, "move(sourceFile): ", readCsvError, sep = ""))
-      })
-    })
-  } else {
-    logger.debug(paset("Skip loading: local file not provided: '", sourceFile, "'", sep = ""))
-  }
-  
-  input
-}
+# for reading files from disk
+source("Helper.R")
 
 rFunction = function(
     settings=NULL,                # MoveApps settings (unused)
@@ -43,7 +20,7 @@ rFunction = function(
   cloudSource <- NULL
   result <- NULL
   if (! is.null(fileName)) {
-    cloudSource <- readLocalFile(paste(cloudFileLocalFolder,"/",fileName,sep = ""))
+    cloudSource <- readInput(paste(cloudFileLocalFolder,"/",fileName,sep = ""))
     result <- cloudSource
     logger.info("Successfully read file from cloud provider (locally).")
   }

ff23fd7ac090fbba7131488a8439177c1b801cb2
andreakoelzsch <akoelzsch@ab.mpg.de>
2023-12-11 13:56:21 +0100

    update according to LocalUploadApp
    only use the data_geese_move2.rds as input file!
    
    error remains that units of attributes dont match (ask BartK)

--- RFunction.R
+++ RFunction.R
@@ -34,91 +34,139 @@ rFunction = function(
   cloudSource <- NULL
   result <- NULL
   
-  if (! is.null(fileName)) {   
-       cloudSource <- tryCatch({
-        # 1: try to read input as (any) RDS file
-        readRdsInput(paste(cloudFileLocalFolder,"/",fileName,sep = ""))
-      },
-      error = function(readRdsError) {
-        tryCatch({
-          # 2 (fallback): try to read input as CSV file in format as specified above (Movebank col. names not required any more)
-   
-          csvSource <-  vroom::vroom(paste(cloudFileLocalFolder,"/",fileName,sep = ""), delim = ",") # alternative to read.csv, takes care of timestamps, default timezome is UTC, problems with empty table avoided above
-          if(length(grep(",",coords)) %in% c(1,2)){
-            coo <- trimws(strsplit(as.character(coords),",")[[1]])
-            logger.info(paste("You have defined as coordinate columns:",coords))
-          } else {
-            coo <- NULL
-            logger.info("An incorrect number of coordinate columns is provided; expecting two (x,y) or three (x,y,z). Cannot transform csv data to move2.")
-            csvSource <- NULL
-          }
-          if(!is.null(csvSource)){
-            logger.info("New data uploaded from csv file, this file is expected to be comma delimited.")
-            logger.info(paste("You have defined as datetime column: ",time_col,".", " The expected timestamp format is 'yyyy-mm-dd HH:MM:SS' and in UTC timezone"))
-            logger.info(paste("You have defined as track ID column: ",track_id_col,"."))
-            logger.info(paste("You have defined as track attributes: ",track_attr,"."))
-            logger.info(paste("You have defined as projection (crs): ",crss,".", " The expected projection is a valid numeric EPSG value. For more info see https://epsg.io/ and https://spatialreference.org/"))
-            
-            #this only if a comma in the data!
-            if(length(grep(",",track_attr))>0){tr_attr <- trimws(strsplit(as.character(track_attr),",")[[1]])} else {tr_attr <- track_attr}
-            
-            # transform data.frame to move2 object
-            csvSource <- dplyr::filter(csvSource,!is.na(csvSource[[track_id_col]])) # if there is a NA in the track_id col, mt_as_move2() gives error
-            n.trcolna <- length(which(is.na(csvSource[[track_id_col]])))
-            if (n.trcolna>0) logger.info(paste("Your tracks contained",ntrcolna,"locations with unspecified track ID. They have been removed."))
-            cloudSource <- mt_as_move2(csvSource,
-                                time_column=time_col,
-                                track_id_column=track_id_col,
-                                track_attributes=track_attr,
-                                coords=coo,
-                                crs=crss,
-                                na.fail=FALSE)
-            ## remove empty locations
-            cloudSource <- cloudSource[!sf::st_is_empty(cloudSource),]  
-            if(nrow(cloudSource)==0){logger.info("Your uploaded csv file does not contain any location data.")}
-            
-            ## remove duplicated timestamps
-            if(!mt_has_unique_location_time_records(cloudSource)){
-              n_dupl <- length(which(duplicated(paste(mt_track_id(cloudSource),mt_time(cloudSource)))))
-              logger.info(paste("Your data has",n_dupl, "duplicated location-time records. We removed here those with less info and then select the first if still duplicated."))
-              ## this piece of code keeps the duplicated entry with least number of columns with NA values
-              cloudSource <- cloudSource %>%
-                mutate(n_na = rowSums(is.na(pick(everything())))) %>%
-                arrange(n_na) %>%
-                mt_filter_unique(criterion='first') # this always needs to be "first" because the duplicates get ordered according to the number of columns with NA. 
-            }
-            
-            ## ensure timestamps are ordered within tracks
-            cloudSource <- dplyr::arrange(cloudSource, mt_track_id(cloudSource), mt_time(cloudSource)) 
-            
-            # make names for cloudSource
-            names(cloudSource) <- make.names(names(cloudSource),allow_=TRUE)
-            mt_track_id(cloudSource) <- make.names(mt_track_id(cloudSource),allow_=TRUE)
-          }
-
-          # first clean order of file as move2() needs it, just to prevent some errors
-          #csvSource <- read.csv(paste(cloudFileLocalFolder,"/",fileName,sep = ""),header=TRUE)
-          #o <- order(csvSource$individual.local.identifier,as.POSIXct(csvSource$timestamp))
-          #mt_as_move2(csvSource[o,],time_column="timestamp",track_id_column="individual.local.identifier",coords=c("location.long","location.lat"),crs="WGS84",na.fail=FALSE)
-        },
-        error = function(readCsvError) {
-          # collect errors for report and throw custom error
-          stop(paste(sourceFile, " -> readRDS(sourceFile): ", readRdsError, "mt_as_move2(sourceFile): ", readCsvError, sep = "")) #this has to be adapted by Clemens
-        })
-      })
-       
-    if (any(class(cloudSource)=="MoveStack")) cloudSource <- mt_as_move2(cloudSource) #transform moveStack to move2 object if necessary
+  if (! is.null(fileName)) { 
+    extn0 <- strsplit(fileName,"[.]")[[1]]
+    extn <- extn0[length(extn0)]
+    
+    if (extn=="rds")
+    {
+      new1 <- readRdsInput(paste(cloudFileLocalFolder,"/",fileName,sep = ""))
       
+      if(is.na(crs(new1))){
+        new1 <- NULL
+        logger.info("The uploaded rds file does not contain coordinate reference system information. This data set cannot be uploaded")
+      }
+      
+      if(is.null(new1)){logger.info("No new rds data found.")
+      } else { 
+        # if .rds is move2
+        if(any(class(new1)=="move2")){logger.info("Uploaded rds file containes a object of class move2")
+          ## remove empty locations
+          new1 <- new1[!sf::st_is_empty(new1),]  
+          if(nrow(new1)==0){logger.info("Your uploaded csv file does not contain any location data.")}
+          #### applying the same "cleaning" as for the move2 created from csv below, as someone can create a move2 in R and uploaded to moveapps. This object can contain empty coords, duplicates etc
+          ## remove duplicated timestamps
+          if(!mt_has_unique_location_time_records(new1)){
+            n_dupl <- length(which(duplicated(paste(mt_track_id(new1),mt_time(new1)))))
+            logger.info(paste("Your data has",n_dupl, "duplicated location-time records. We removed here those with less info and then select the first if still duplicated."))
+            ## this piece of code keeps the duplicated entry with least number of columns with NA values
+            new1 <- new1 %>%
+              mutate(n_na = rowSums(is.na(pick(everything())))) %>%
+              arrange(n_na) %>%
+              mt_filter_unique(criterion='first') # this always needs to be "first" because the duplicates get ordered according to the number of columns with NA. 
+          }
+          
+          ## ensure timestamps are ordered within tracks
+          new1 <- dplyr::arrange(new1, mt_track_id(new1), mt_time(new1)) 
+        }
+        
+        # if move2 object - fine, else transform moveStack to move2
+        if(any(class(new1)=="MoveStack")){
+          new1 <- mt_as_move2(new1)
+          logger.info("Uploaded rds file containes a object of class moveStack that is transformed to move2.")
+        }
+        if(!any(class(new1)%in%c("move2","MoveStack"))){
+          new1 <- NULL
+          logger.info("Uploaded rds file contains data with unexpected class, please ensure that the file contains a object of class 'move2' or 'MoveStack'.")}
+      }
+      # make names for new1
+      if(!is.null(new1)){
+        names(new1) <- make.names(names(new1),allow_=TRUE)
+        mt_track_id(new1) <- make.names(mt_track_id(new1),allow_=TRUE)
+      }
+      cloudSource <- new1
+
+    } else if (extn == "csv")
+    {
+      test <- readLines(paste(cloudFileLocalFolder,"/",fileName,sep = "")) #to check if it is empty, only continue if not
+      if (test[1]=="")
+      {
+        df2 <- NULL
+        logger.info("No new csv data found. The settings of the App are not used.")
+        
+      } else
+      {
+        df2 <-  vroom::vroom(paste(cloudFileLocalFolder,"/",fileName,sep = ""), delim = ",") # alternative to read.csv, takes care of timestamps, default timezome is UTC, problems with empty table avoided above
+        if(length(grep(",",coords)) %in% c(1,2)){
+          coo <- trimws(strsplit(as.character(coords),",")[[1]])
+          logger.info(paste("You have defined as coordinate columns:",coords))
+        } else {
+          coo <- NULL
+          logger.info("An incorrect number of coordinate columns is provided; expecting two (x,y) or three (x,y,z). Cannot transform csv data to move2.")
+          df2 <- NULL
+        }
+        if(!is.null(df2)){
+          logger.info("New data uploaded from csv file, this file is expected to be comma delimited.")
+          logger.info(paste("You have defined as datetime column: ",time_col,".", " The expected timestamp format is 'yyyy-mm-dd HH:MM:SS' and in UTC timezone"))
+          logger.info(paste("You have defined as track ID column: ",track_id_col,"."))
+          logger.info(paste("You have defined as track attributes: ",track_attr,"."))
+          logger.info(paste("You have defined as projection (crs): ",crss,".", " The expected projection is a valid numeric EPSG value. For more info see https://epsg.io/ and https://spatialreference.org/"))
+          
+          #this only if a comma in the data!
+          if(length(grep(",",track_attr))>0){tr_attr <- trimws(strsplit(as.character(track_attr),",")[[1]])} else {tr_attr <- track_attr}
+          
+          # transform data.frame to move2 object
+          df2 <- dplyr::filter(df2,!is.na(df2[[track_id_col]])) # if there is a NA in the track_id col, mt_as_move2() gives error
+          n.trcolna <- length(which(is.na(df2[[track_id_col]])))
+          if (n.trcolna>0) logger.info(paste("Your tracks contained",ntrcolna,"locations with unspecified track ID. They have been removed."))
+          new2 <- mt_as_move2(df2,
+                              time_column=time_col,
+                              track_id_column=track_id_col,
+                              track_attributes=track_attr,
+                              coords=coo,
+                              crs=crss,
+                              na.fail=FALSE)
+          ## remove empty locations
+          new2 <- new2[!sf::st_is_empty(new2),]  
+          if(nrow(new2)==0){logger.info("Your uploaded csv file does not contain any location data.")}
+          
+          ## remove duplicated timestamps
+          if(!mt_has_unique_location_time_records(new2)){
+            n_dupl <- length(which(duplicated(paste(mt_track_id(new2),mt_time(new2)))))
+            logger.info(paste("Your data has",n_dupl, "duplicated location-time records. We removed here those with less info and then select the first if still duplicated."))
+            ## this piece of code keeps the duplicated entry with least number of columns with NA values
+            new2 <- new2 %>%
+              mutate(n_na = rowSums(is.na(pick(everything())))) %>%
+              arrange(n_na) %>%
+              mt_filter_unique(criterion='first') # this always needs to be "first" because the duplicates get ordered according to the number of columns with NA. 
+          }
+          
+          ## ensure timestamps are ordered within tracks
+          new2 <- dplyr::arrange(new2, mt_track_id(new2), mt_time(new2)) 
+          
+          # make names for new2
+          names(new2) <- make.names(names(new2),allow_=TRUE)
+          mt_track_id(new2) <- make.names(mt_track_id(new2),allow_=TRUE)
+      }
+    }
+    if(is.null(df2)) new2 <- NULL else if (nrow(new2)==0) new2 <- NULL
+    cloudSource <- new2
+    
+  } else 
+  {
+      logger.info("The extension of your uploaded file is neither `csv` nor `rds`. This App cannot work with other files, NULL will be returned.")
+      cloudSource <- NULL
+  }
+    
     result <- cloudSource
-    logger.info("Successfully read file from cloud provider (locally).")
   }
   
   if (exists("data") && !is.null(data)) {
-    logger.info("Merging input from prev. app and cloud file together.")
-    result <- mt_stack(cloudSource, data, .track_combine="rename",.track_id_repair="universal") #how is it with the timezones in this function? Do we have to force a fix here, again?
+    logger.info("Merging input from previous App and cloud file together.")
+    result <- mt_stack(result, data, .track_combine="rename",.track_id_repair="universal") #how is it with the timezones in this function? Do we have to force a fix here, again?
     
   } else {
-    logger.info("No input from prev. app provided, nothing to merge. Will deliver the mapped cloud-file only.")
+    logger.info("No input from previous App provided, nothing to merge. Will deliver the mapped cloud-file only.")
   }
   
   logger.info("I'm done.")
